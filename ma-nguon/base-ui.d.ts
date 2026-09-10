7e9ab01ff16469d3b887f89982f9ad8968939f95
Co Vua Sai Gon <admin@covuosaigon.com>
2026-09-10 21:32:52 +0700

    fix: convert project ma-nguon to pure nextjs 16 app router

---
+++ ma-nguon/base-ui.d.ts
@@ -0,0 +1,35 @@
+declare module '@base-ui/react' {
+  export const Combobox: any;
+  export namespace Combobox {
+    export namespace Value { export type Props = any; }
+    export namespace Trigger { export type Props = any; }
+    export namespace Clear { export type Props = any; }
+    export namespace Input { export type Props = any; }
+    export namespace Popup { export type Props = any; }
+    export namespace Positioner { export type Props = any; }
+    export namespace Portal { export type Props = any; }
+    export namespace Backdrop { export type Props = any; }
+    export namespace Arrow { export type Props = any; }
+    export namespace List { export type Props = any; }
+    export namespace Item { export type Props = any; }
+    export namespace ItemText { export type Props = any; }
+    export namespace ItemIndicator { export type Props = any; }
+    export namespace Group { export type Props = any; }
+    export namespace GroupLabel { export type Props = any; }
+    export namespace Empty { export type Props = any; }
+    export namespace Description { export type Props = any; }
+    export namespace Root { export type Props = any; }
+    export namespace Collection { export type Props = any; }
+    export namespace Control { export type Props = any; }
+    export namespace Content { export type Props = any; }
+    export namespace Separator { export type Props = any; }
+    export namespace Status { export type Props = any; }
+    export namespace Icon { export type Props = any; }
+    export namespace Chips { export type Props = any; }
+    export namespace Chip { export type Props = any; }
+    export namespace ChipRemove { export type Props = any; }
+    export namespace Row { export type Props = any; }
+  }
+  const content: any;
+  export default content;
+}
